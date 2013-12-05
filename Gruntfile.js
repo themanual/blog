c5945b34d50b7d56aa7ef95ba9802547b7ff93d4
Paulo Pereira <paulo.zoom@gmail.com>
2013-12-05 17:49:55 +0100

    Add grunt-sass-contrib and update gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,26 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // Sass
+    sass: {
+      // Expanded, line numbers
+      dev: {
+        options: {
+          style:        'expanded',
+          lineNumbers:  'true',
+          loadPath:     ['bower_components']
+        },
+        files: {
+          'assets/css/main.css': 'assets/scss/main.scss'
+        }
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
   // Default task.
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['sass:dev']);
 
 };
