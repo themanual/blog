58321413f3a09d03c12508a2d5e784245e3abffb
Paulo Pereira <paulo.zoom@gmail.com>
2013-12-16 19:19:29 +0000

    Add GA via analytics.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 /* global jQuery */
+/* global analytics */
 
 (function($) {
   $('.blog-post-body').fitVids();
 })(jQuery);
+
+// Analytics Init
+analytics.initialize({
+  'Google Analytics': {
+    trackingId: 'UA-46481984-1'
+  }
+});
