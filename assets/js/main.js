74c0f0de04b28fb0daededa73b0cfb3336e914c0
Paulo Pereira <paulo.zoom@gmail.com>
2013-12-16 19:20:07 +0000

    Move twitter widget script to JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,13 @@ analytics.initialize({
     trackingId: 'UA-46481984-1'
   }
 });
+
+// Twitter Init
+window.twttr = (function (d,s,id) {
+  var t, js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+  js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+  return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+}(document, "script", "twitter-wjs"));
+
+
