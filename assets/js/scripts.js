125e56151783f46be0c3ea3aa05d381cb166385f
Paulo Pereira <paulo.zoom@gmail.com>
2013-12-17 02:39:08 +0000

    Revert "Use HTML google analytics code to debug."
    This reverts commit 7f1d0253f5f2ac4f61d67fadc307b54022d5d7e2.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8911,13 +8911,13 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
 })(jQuery);
 
 // GA
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-// ga('create', 'UA-46481984-1', 'auto');
-// ga('send', 'pageview');
+ga('create', 'UA-46481984-1', 'auto');
+ga('send', 'pageview');
 
 // Twitter Init
 window.twttr = (function (d,s,id) {
@@ -8934,13 +8934,13 @@ window.twttr = (function (d,s,id) {
 // Twitter Follow
 twttr.ready(function (twttr) {
   twttr.events.bind('follow', function(intent_event) {
-    // ga('send', 'social', 'twitter', 'follow');
+    ga('send', 'social', 'twitter', 'follow');
   });
 });
 
 // Form Subscription
 $('#sidebar-subscribe-form').submit(function() {
-  // ga('send', 'event', 'blog newsletter', 'subscribed');
+  ga('send', 'event', 'blog newsletter', 'subscribed');
 });
 
 // Shop Visit
